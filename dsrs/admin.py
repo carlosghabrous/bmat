4d03eacc7304d9eb2c0530ccfe6af03e54095cd1
carlosghabrous <carlos.ghabrous@gmail.com>
2021-03-02 21:59:47 +0100

    Add models to admin

--- dsrs/admin.py
+++ dsrs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Territory, Currency, DSR, DSP
 
 # Register your models here.
+
+admin.site.register(Territory)
+admin.site.register(Currency)
+admin.site.register(DSR)
+admin.site.register(DSP)
