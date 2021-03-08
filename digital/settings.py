a691140e5c48f287774e893653faeedcb0c2dd4f
carlosghabrous <carlos.ghabrous@gmail.com>
2021-03-08 14:14:14 +0100

    Add localhost to the ALLOWED_HOSTS

--- digital/settings.py
+++ digital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "cmucgwyq^&j8rn!i(2v2+l(e13_63o9+3i1d$hm5r0blrok$27"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
